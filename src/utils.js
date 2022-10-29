0768fec718ef9c7d7b4cda74c8efa9ec184e4bd7
sai1781 <96867450+sai1781@users.noreply.github.com>
2022-10-29 16:48:30 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const omdb = axios.create({
-    baseURL:"http://www.omdbapi.com/",
+    baseURL:"https://www.omdbapi.com/",
     timeout: 15000,
     params: {
         apikey:"e09df99a"
